bad865360caa39dce4002d21043d10474bcf20a3
Ron Fletcher <ronfletcher1@Mighty-NOR.local>
2018-11-16 14:49:41 -0500

    python loop exercise n_to_m

---
+++ m_to_n.py
@@ -0,0 +1,5 @@
+first_ten = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+start_num = int(raw_input ("Enter Start Number"))
+end_num = int(raw_input ("Enter End Number"))
+for first_ten in range (start_num, end_num + 1):
+    print first_ten
