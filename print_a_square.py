f52ac80676a8f5a78bc1fb686fd54f3a5b90e742
Ron Fletcher <ronfletcher1@Mighty-NOR.local>
2018-11-21 08:53:31 -0500

    print a square exercise with help

---
+++ print_a_square.py
@@ -0,0 +1,17 @@
+# print "*****"
+# print "*****"
+# print "*****"
+# print "*****"
+# print "*****"
+
+width = 8
+n = 6
+i = 0
+j = 0
+# line = ""
+for i in range(0,width):
+    line = ""
+    line = line + "*"
+    print i
+for j in range(0,n):
+    print line
